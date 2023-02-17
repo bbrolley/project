87a03fc2af2782f1f0e6bc568026750f184a46ec
bbrolley <barry.brolley@businesspartner.roche.com>
2023-02-17 18:27:39 +0000

    first commit

---
+++ credentials.R
@@ -0,0 +1,4 @@
+gitcreds::gitcreds_set()
+# And enter the created GH-Token in the R console when prompted
+usethis::use_git_config(user.name = "bbrolley",
+                        user.email = "barry.brolley@businesspartner.roche.com")
